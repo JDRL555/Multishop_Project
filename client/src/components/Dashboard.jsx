0a97340418c300f46df863adbad2e1aef1aad77e
gabrieljustdance <gabriel2043579@gmail.com>
2023-12-06 18:27:52 -0400

    style: changed background color to white

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, {useState} from "react";
 
 export default function Dashboard() {
+  
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleDropdown = () => setIsOpen(!isOpen);
+
   return (
-    <>
+  <div>
+      <h1>hola</h1>
+        <div className="dropdown">
+          <button onClick={toggleDropdown}>Trigger</button>
+          {isOpen && (
+            <ul className="dropdown-content">
+              <li>Option 1</li>
+              <li>Option 2</li>
+              <li>Option 3</li>
+            </ul>
+            )}
+         </div>
+  </div>
     
-    <h1>hola</h1>
-    
-    </>
 
   )
 
