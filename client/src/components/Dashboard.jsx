27813644191b4f456adbb6670ee4c328e1ddf80b
gabrieljustdance <gabriel2043579@gmail.com>
2023-12-07 20:36:48 -0400

    feat: created the first admin page structure

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,23 +1,8 @@
 import React, {useState} from "react";
 export default function Dashboard() {
   
-  const [isOpen, setIsOpen] = useState(false);
-
-  const toggleDropdown = () => setIsOpen(!isOpen);
-
   return (
-    <div>
-        <h1>hola</h1>
-          <div className="dropdown">
-            <button onClick={toggleDropdown}>Trigger</button>
-            {isOpen && (
-              <ul className="dropdown-content">
-                <li>Option 1</li>
-                <li>Option 2</li>
-                <li>Option 3</li>
-              </ul>
-              )}
-          </div>
-    </div>
+   <>
+   </>
   )
 }
---
+++ client/src/components/Show.jsx
@@ -0,0 +1,21 @@
+import React, { useState }  from 'react'
+import Modal from './Modal'
+import "../styles/Modal.css"
+
+
+export default function Show() {
+  
+
+  return (
+    <>      
+        <Modal>
+          <div id='Create'>
+          <form id='tabla' action="">
+           muestra
+            <button type="button">crear</button>
+          </form>
+          </div>
+        </Modal>
+    </>
+  )
+}
